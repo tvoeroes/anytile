a44b3b71dcc9b3ca1f77aaa93b751602a481bf1a
Tomaž Vöröš <tomaz.voros@gmail.com>
2026-03-18 21:41:13 +0100

    Add minimap

--- src/AnytileXyzView.ts
+++ src/AnytileXyzView.ts
@@ -583,5 +583,34 @@ export class AnytileXyzView extends HTMLElement
 			if (this.#menu.coords)
 				this.#drawTile(null, tile.id, yt, xt)
 		}
+
+		if (this.#menu.minimap)
+			this.#drawMinimap()
+	}
+
+	#drawMinimap()
+	{
+		const size = 128
+		const margin = 8
+		const mx = this.#canvas.width - size - margin
+		const my = margin
+
+		this.#ctx.fillStyle = "white"
+		this.#ctx.fillRect(mx, my, size, size)
+
+		this.#ctx.strokeStyle = "black"
+		this.#ctx.lineWidth = 1
+		this.#ctx.strokeRect(mx + 0.5, my + 0.5, size - 1, size - 1)
+
+		const cx = Math.round(mx + this.#menu.x * (size - 1)) + 0.5
+		const cy = Math.round(my + this.#menu.y * (size - 1)) + 0.5
+		const armLen = 4
+
+		this.#ctx.beginPath()
+		this.#ctx.moveTo(cx - armLen, cy)
+		this.#ctx.lineTo(cx + armLen, cy)
+		this.#ctx.moveTo(cx, cy - armLen)
+		this.#ctx.lineTo(cx, cy + armLen)
+		this.#ctx.stroke()
 	}
 }
